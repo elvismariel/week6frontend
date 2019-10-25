a0e9a733d5d1f5f7893223cd3101d5fd641b26f0
Elvis <elvismariel@gmail.com>
2019-10-24 22:51:43 -0300

    Consumindo a api de criacao de box

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
+import api from "../../services/api"
 
 import logo from "../../assets/logo.svg";
 
 import "./styles.css";
 
 export default class Main extends Component {
+
+    state = {
+        newBox: ""
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        console.log(this.state.newBox);
+
+        const response = await api.post("boxes", {
+            title: this.state.newBox,
+        });
+
+        console.log(response.data)
+    };
+
+    handleInputChange = (e) => {
+        this.setState({newBox: e.target.value});
+    };
+
     render(){
         return (
             <div id="main-container">
-                <form action="">
+                <form onSubmit={this.handleSubmit}>
                     <img src={logo} alt="" />
-                    <input placeholder="Criar um box" />
+                    <input value={this.state.newBox} placeholder="Criar um box" onChange={this.handleInputChange}/>
                     <button type="submit">Criar</button>
                 </form>
             </div>
